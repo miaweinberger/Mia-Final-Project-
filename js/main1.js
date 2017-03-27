d01a407d1df4e7a2ab85a1dd6786be1bb0a0909b
miaweinberger <mia.e.weinberger@gmail.com>
2017-03-26 21:44:49 -0400

    3 cards added

--- js/main1.js
+++ js/main1.js
@@ -4,24 +4,79 @@ var protests = [
   {
     name: 'City of Refuge: 24 Hour Action for Refugees',
     neighborhood: 'financial district', 
-    issue: 'immigration'
+    issue: 'immigration',
+    date: 'march 28 (tuesday)',
+    link: 'https://www.facebook.com/events/1899030313649428/'
   } , {
     name: 'Cesar Chavez RALLY for New York State Farmworker Rights',
     neighborhood: 'lower manhattan',
-    issue: 'immigration'
+    issue: 'immigration',
+    tags: ['immigration', 'lowerManhattan'],  
+    date: 'march 28 (tuesday)',
+    link: 'https://www.facebook.com/events/1381782628547228/'
   }, {
     name: 'ACT UP: 30th Anniversary March and Rally',
     neighborhood: 'greenwich village', 
-    issue: 'health care'
+    issue: 'health care',
+    date: 'march 28 (tuesday)',
+    link: 'https://www.facebook.com/events/204115296734664/'
   } 
 ]
 
-$('#finder').click(showResults);
+$('#finder').click(displayCards);
 
-function showResults() {
-// $('#results').append(protests[0].name);
-$('.header').html(protests[0].name);
+
+//console log each item in array
+
+function displayCards() {
+  protests.forEach(function(item, index) {
+    console.log(protests[index].name);
+    console.log(protests[index].neighborhood);
+    console.log(protests[index].issue);
+    console.log(protests[index].date);
+   // var currentHeader = $('.header')[index];
+   var currentDate = protests[index].date;
+   var currentName = protests[index].name;
+   var currentNeighborhood = protests[index].neighborhood;
+   var currentIssue = protests[index].issue;
+   var currentLink = protests[index].link;
+   var currentTagArray = protests[index].tags;
+   var currentTags;
+   
+   currentTagArray.forEach(function(item, index) {
+    
+   });
+   // var currentLink = $('.link')[index];
+   // console.log('X: ', item.name);
+   // $(currentHeader).html(item.name);
+   // $(currentDate).html(item.date);
+   // $(currentLink).html(item.link);
+
+   var card = `
+      <div class="ui raised card ${currentTags}">
+        <div class="content">
+          <div class="header">${currentName}</div>
+          <div class="meta">
+            <span class="date">${currentDate}</span>
+            <a href="${currentLink}" target="_blank">more info</a>
+          </div>
+          <div class="description">
+            <p></p>
+          </div>
+        </div>
+      </div>
+`;
+
+   $('#results').append(card);
+
+  });
 }
 
+
+
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows'
+});
 
 });
